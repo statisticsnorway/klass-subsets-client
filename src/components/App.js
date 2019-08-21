94b8cc4d3ad985755a0c965d8bb964e0f982e08d
kons_lapi <alina.lapina@ssb.no>
2019-08-21 15:34:53 +0200

    Styling and content updated. Create-react-app related styling and content removed.

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from '../images/logo.svg';
+import logo from '../images/SSB_logo.png';
 import '../css/App.css';
+import WelcomePage from "./WelcomePage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div className="App-name">
+                    <img src={logo} className="App-logo" alt="SSB logo"/>
+                    Klass uttrekk admin
+                </div>
+            </header>
+            <WelcomePage/>
+        </div>
+    );
 }
-
-export default App;
---
+++ src/components/WelcomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/pages.css';
+
+export default function WelcomePage() {
+    return (
+        <div className="page">
+            <h1>Welcome to Klass Subsets web</h1>
+            <p>Simple paragraph</p>
+            <h2>Header h2</h2>
+            <p>Simple paragraph</p>
+            <h3>Header h3</h3>
+            <p>Simple paragraph</p>
+        </div>
+    )
+}
