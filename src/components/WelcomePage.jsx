0510b49a2a522d7a2be67d693057a1bfff391c1d
kons_lapi <alina.lapina@ssb.no>
2019-08-21 15:44:21 +0200

    Meaningful text on the welcome page. Removed blue background.

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -5,11 +5,15 @@ export default function WelcomePage() {
     return (
         <div className="page">
             <h1>Welcome to Klass Subsets web</h1>
-            <p>Simple paragraph</p>
-            <h2>Header h2</h2>
-            <p>Simple paragraph</p>
-            <h3>Header h3</h3>
-            <p>Simple paragraph</p>
+            <p>The application is under development.</p>
+            <h2>Source code</h2>
+            <p>You can find the source code on <a
+                    className="App-link"
+                    href="https://github.com/statisticsnorway/klass-subsets-web>GitHub"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >GitHub repository</a>
+            </p>
         </div>
     )
 }
