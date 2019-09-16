53a277ae3637fd54854c65ae40c097e4c1f98d52
kons_lapi <alina.lapina@ssb.no>
2019-09-16 15:03:08 +0200

    reformat code

--- src/components/SubsetForm.jsx
+++ src/components/SubsetForm.jsx
@@ -7,36 +7,36 @@ export default function SubsetForm() {
 
     return (
         <div className="page">
-                <h2>Create new subset</h2>
-                <h3>Norwegian name</h3>
-                <input type="text" value={subset.draft.names}
-                       onChange={(e) => { subset.dispatch({
-                               action: "names",
-                               data: e.target.value
-                           });
-                       }}/>
+            <h2>Create new subset</h2>
+            <h3>Norwegian name</h3>
+            <input type="text" value={subset.draft.names}
+                   onChange={(e) => { subset.dispatch({
+                           action: "names",
+                           data: e.target.value
+                       });
+                   }}/>
 
-                <h3>Norwegian description</h3>
-                <input type="text" value={subset.draft.description}
-                       onChange={(e) => { subset.dispatch({
-                               action: "description",
-                               data: e.target.value
-                           });
-                       }}/>
+            <h3>Norwegian description</h3>
+            <input type="text" value={subset.draft.description}
+                   onChange={(e) => { subset.dispatch({
+                           action: "description",
+                           data: e.target.value
+                       });
+                   }}/>
 
-                <h3>Owner</h3>
-                <input type="text" value={subset.draft.ownerId}
-                       onChange={(e) => { subset.dispatch({
-                               action: "ownerId",
-                               data: e.target.value
-                           });
-                       }}/>
+            <h3>Owner</h3>
+            <input type="text" value={subset.draft.ownerId}
+                   onChange={(e) => {subset.dispatch({
+                           action: "ownerId",
+                           data: e.target.value
+                       });
+                   }}/>
 
-                <br/><br/>
+            <br/><br/>
 
-                <button onClick={() => {subset.dispatch({action: "empty"});}}>Empty</button>
-                <button onClick={() => {subset.dispatch({action: "reset"});}}>Reset</button>
-                <button onClick={() => { console.log("Submit subset draft: ", subset.draft) }}>Submit</button>
+            <button onClick={() => {subset.dispatch({action: "empty"});}}>Empty</button>
+            <button onClick={() => {subset.dispatch({action: "reset"});}}>Reset</button>
+            <button onClick={() => {console.log("Submit subset draft: ", subset.draft)}}>Submit</button>
         </div>
     );
 }
