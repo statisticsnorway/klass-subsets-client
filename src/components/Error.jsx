be89e1bc24de97b946abedbd8d42513780006323
alina-lapina <Zaq12wsx->
2019-09-17 16:52:37 +0200

    dismiss all button

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -7,18 +7,19 @@ export default function Error() {
     return (
         <>
             { errorRegister.errors.map((error, index) => (
-                <div key={index} >
+                <div key={index}>
                     <span style={{color: "red"}}>{`${error.status}: ${error.message}`}</span>
                     <button
-                        style={{color:"red", background:"none", border:"none", fontWeight: "400"}}
-                        onClick={(e) => {
-                            //e.target.parentElement.remove();
-                            errorRegister.dispatch({action: "remove", data: index})
-                        }}
+                        style={{color: "red", background: "none", border: "none", fontWeight: "600"}}
+                        onClick={(e) => {errorRegister.dispatch({action: "remove", data: index})}}
                     >x
                     </button>
                 </div>
             ))}
+            { errorRegister.errors.length > 1 &&
+                <button onClick={(e) => {errorRegister.dispatch({action: "empty"})}}
+                >Dismiss all</button>
+            }
         </>
     );
 }
