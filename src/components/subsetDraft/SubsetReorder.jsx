d2be899e8b2d92616e308084561029bf0a991d7d
kons_lapi <alina.lapina@ssb.no>
2019-09-19 09:38:14 +0200

    removed code related to Drag-able List Reordering

--- src/components/subsetDraft/SubsetReorder.jsx
+++ src/components/subsetDraft/SubsetReorder.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import "../../css/lists.css";
-import ReorderView from "../../utils/dragableListReordering"
 
 export const SubsetReorder = ({subset}) => {
 
@@ -8,7 +6,6 @@ export const SubsetReorder = ({subset}) => {
         <>
             <h3>Subset reorder</h3>
             <button onClick={() => {console.log("Reorder subset: ", subset.draft);}}>Show codes</button>
-            <ReorderView listData={subset.draft.codes}/>
             <br/><br/>
         </>
     );
