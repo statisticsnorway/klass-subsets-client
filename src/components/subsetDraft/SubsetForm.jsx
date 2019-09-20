b12e0e50add44078a67a3aae6565a121c1a4ebfd
kons_lapi <alina.lapina@ssb.no>
2019-09-20 09:41:16 +0200

    pull from development (reordering) and feature/form-flow_KF-303 (step labels), raw draft data on the Preview page, forgotten import

--- src/components/subsetDraft/SubsetForm.jsx
+++ src/components/subsetDraft/SubsetForm.jsx
@@ -1,7 +1,8 @@
 import React, {useContext} from "react";
-import "../css/pages.css";
-import {AppContext} from "../controllers/context";
-import {Navigation, Step} from "../utils/navigation";
+import "../../css/pages.css";
+import {AppContext} from "../../controllers/context";
+import {Navigation, Step} from "../../utils/navigation";
+import {SubsetReorder} from "./SubsetReorder";
 
 export default function SubsetForm() {
     const {subset} = useContext(AppContext);
@@ -33,6 +34,7 @@ export const SubsetPreview = ({subset}) => {
     return (
         <>
             <h3>Subset preview</h3>
+            <pre>{JSON.stringify(subset.draft, null, 4)}</pre>
             <button onClick={() => {console.log("Publish subset: ", subset.draft);}}>Publish</button>
             <br/><br/>
         </>
