2f6d450f0f6cf081b89bd4f86611195ccb40ab2e
kons_lapi <alina.lapina@ssb.no>
2019-10-05 17:31:07 +0200

    fieldsets

--- src/components/subsetDraft/SubsetMetadata.jsx
+++ src/components/subsetDraft/SubsetMetadata.jsx
@@ -6,35 +6,30 @@ export const SubsetMetadata = ({subset}) => {
         <>
             <br/><br/>
 
-            <label htmlFor="name" style={{display:"block"}}>Name</label>
-                <input type="text" id="name" value={subset.draft.names}
-                   onChange={(e) => { subset.dispatch({
-                       action: "names",
-                       data: e.target.value });
-                   }}/>
-                <select name="language">
-                    <option value="nb" defaultValue >Bokmål</option>
-                    <option value="nn">Nynorsk</option>
-                    <option value="en">English</option>
-                </select>
-                <button style={{margin: "20px"}}>+</button>
+            <NameFieldset names="Uttrekk for ..."/>
 
             <br/><br/>
 
-            <label htmlFor="owner" style={{display:"block"}}>Owner</label>
+            <fieldset>
+                <label htmlFor="owner" style={{display:"block"}}>Owner</label>
                 <input type="text" id="owner" value={subset.draft.ownerId}
                    onChange={(e) => {subset.dispatch({
                        action: "ownerId",
                        data: e.target.value });
                    }}/>
+            </fieldset>
 
             <br/><br/>
 
+            <fieldset>
             <label style={{display:"block"}}>Valid period</label>
             <label>From:<input type="date"/></label>
             <label>To:<input type="date"/></label>
+            </fieldset>
 
-            <br/><br/>
+            <br/>
+
+            <fieldset>
             <label>Subject
             <select style={{margin: "10px"}}>
                 <option value="Economics" selected={true}>Economics</option>
@@ -42,9 +37,11 @@ export const SubsetMetadata = ({subset}) => {
                 <option value="Work" >Economics</option>
             </select>
             </label>
+            </fieldset>
 
-            <br/><br/>
+            <br/>
 
+            <fieldset>
             <label htmlFor="description" style={{display:"block"}}>Description</label>
             <input type="textarea" id="description"
                    value={subset.draft.description}
@@ -58,8 +55,9 @@ export const SubsetMetadata = ({subset}) => {
                 <option value="en">English</option>
             </select>
             <button style={{margin: "20px"}}>+</button>
+            </fieldset>
 
-            <br/><br/>
+            <br/>
 
             <label><input type="checkbox"/>Subscribe for changes</label>
 
@@ -70,5 +68,35 @@ export const SubsetMetadata = ({subset}) => {
 
             <br/><br/>
         </>
+    );
+};
+
+export const NameFieldset = ({names = "Uttrekk for ...",
+                             handle = (data) => console.log(data),
+                             languages = []}) => {
+    return (
+        <fieldset>
+            <label htmlFor="name" style={{display: "block"}}>Name</label>
+            <input type="text" id="name" value={names}
+                   onChange={(e) => {
+                       //subset.dispatch({action: "names", data: e.target.value});
+                       handle(e);
+                   }}/>
+            <LanguageSelect languages={[
+               {value: "nb", option: "Norsk bokmål"},
+               {value: "no", option: "Nynorks"},
+               {value: "en", option: "English"}]} />
+            <button style={{margin: "20px"}}>+</button>
+        </fieldset>
+    );
+};
+
+export const LanguageSelect = ({languages = []}) => {
+    return (
+        <select name="language">
+            <option value="nb" defaultValue>Bokmål</option>
+            <option value="nn">Nynorsk</option>
+            <option value="en">English</option>
+        </select>
     );
 };
