cc461a045cae58cfa6662c0c7ed3c7227883151b
alina-lapina <Zaq12wsx->
2020-03-05 10:09:09 +0100

    publish response alerted

--- src/components/subsetDraft/SubsetPublish.jsx
+++ src/components/subsetDraft/SubsetPublish.jsx
@@ -1,8 +1,8 @@
 import React, {useEffect} from 'react';
 import '../../css/pages.css';
-import {useTranslation} from "react-i18next";
-import {Button, Title} from "@statisticsnorway/ssb-component-library";
-import {Subset} from "../Subset";
+import {useTranslation} from 'react-i18next';
+import {Button, Title} from '@statisticsnorway/ssb-component-library';
+import {Subset} from '../Subset';
 import {usePost} from '../../controllers/subsets-service';
 
 // TODO: better preview (human pleasant)
@@ -11,16 +11,23 @@ export const SubsetPublish = ({subset}) => {
 
     useEffect(() => subset.dispatch({action: 'remove_empty'}), []);
 
-    const [data, setPayload, isLoading, error, setPath] = usePost();
+    const [data, setPayload, isLoading, error] = usePost();
+    useEffect(() => {
+        console.log('GET subset: ', data);
+        data !== null && alert(`Publishing succeeded: ${JSON.stringify(data)}`);
+    }, [data]);
+    useEffect(() => {
+        console.log('Failed publishing: ', error);
+        error !== null && alert(`Publishing failed: ${JSON.stringify(error)}`);
+    }, [error]);
 
     return (
         <>
             <Title size={3}>{t('Review and publish')}</Title>
             <Subset subset={data || subset.draft}/>
-            {(data || error) && alert('Published', data, error)}
-            <Button disabled={data}
+            <Button disabled={data !== null}
                 onClick={() => {
-                    console.log('Publish subset: ', subset.draft);
+                    console.log('POST subset: ', subset.draft);
                     setPayload(subset.draft);
             }}>{t('Publish')}</Button>
             {isLoading && 'Loading...'}
