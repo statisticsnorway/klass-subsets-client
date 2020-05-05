0c652083bc8ceb401051928184be58b361b1a702
kons_lapi <alina.lapina@ssb.no>
2020-05-05 03:22:16 +0200

    #105 adm detail type ORIGIN is used if no classifications

--- src/components/subsetDraft/SubsetCodes.jsx
+++ src/components/subsetDraft/SubsetCodes.jsx
@@ -24,7 +24,7 @@ export const SubsetCodes = ({subset}) => {
             .find(d => d.administrativeDetailType === 'ORIGIN')
             .values.forEach(v => subset.dispatch({
                 action: 'classifications_prepend_included',
-                data: { urn: v, included: true }
+                data: [{ urn: v, included: true }]
             }))
     }
 
