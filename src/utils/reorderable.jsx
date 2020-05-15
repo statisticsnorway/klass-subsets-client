6900d03771144ad009963d3a3315d839d118bad6
kons_lapi <alina.lapina@ssb.no>
2020-05-15 13:27:55 +0200

    reorder help info, no translation yet

--- src/utils/reorderable.jsx
+++ src/utils/reorderable.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {useTranslation} from 'react-i18next';
-import {Trash2, Repeat, ChevronUp, ChevronDown} from 'react-feather';
+import {Trash2, Repeat, ChevronUp, ChevronDown, HelpCircle} from 'react-feather';
+import {Paragraph, LeadParagraph} from '@statisticsnorway/ssb-component-library';
 import {useCode} from '../controllers/klass-api';
 import '../css/form.css';
 
@@ -9,6 +10,7 @@ export const Reorderable = ({list = [], rerank, remove}) => {
 
     const [dropTarget, setDropTarget] = useState();
     const [dragTargets, setDragTargets] = useState([]);
+    const [showHelp, setShowHelp] = useState(false);
 
     return (
         <>
@@ -21,9 +23,43 @@ export const Reorderable = ({list = [], rerank, remove}) => {
                     <th>{t('Code')}</th>
                     <th style={{textAlign: 'right'}}>{t('Classification')}</th>
                     <th>{t('Code name')}</th>
-                    <th style={{textAlign: 'center'}} colSpan='2'>{t('Rank')}</th>
+                    <th style={{textAlign: 'center'}}
+                        colSpan='2'>
+                        {t('Rank')}
+                        <button onClick={(event) => {
+                            event.stopPropagation();
+                            setShowHelp(prev => !prev);
+                        }}>
+                            <HelpCircle color='#2D6975'/>
+                        </button>
+                    </th>
                     <th className='for_screen_readers'>{t('Remove')}</th>
                 </tr>
+                {showHelp &&
+                <tr>
+                    <td colSpan='5'
+                        style={{height: '100px', background: '#274247', color: 'white'}}>
+                        <LeadParagraph negative>{t('Code rank help intro')}</LeadParagraph>
+                        <ul>
+                            <li><Paragraph negative>{t('Code rank help drag-and-drop')}</Paragraph></li>
+                            <li><Paragraph negative>
+                            <input className='rank' name='example'
+                                   style={{textAlign: 'left', width: '35px', padding: '7px 5px'}}
+                                   value='25' disabled />
+                            {t('Code rank help input')}
+                            <Repeat color='#62919A'/>
+                            {t('Code rank help input reset')}
+                        </Paragraph></li>
+                            <li><Paragraph negative>
+                            <span style={{display: 'inline-block', width: '20px'}}>
+                                <ChevronUp size={16} color='#1A9D49'/>
+                                <ChevronDown size={16} color='#1A9D49'/>
+                            </span>
+                            {t('Code rank help arrows')}
+                        </Paragraph></li>
+                        </ul>
+                    </td>
+                </tr>}
                 {list.sort((a, b) => (a.rank - b.rank -1))
                     .map((item, i) => (
                         <ReordableItem key={item.urn+i}
@@ -137,13 +173,7 @@ export const ReordableItem = ({item = {}, remove, rerank, onDragEnd, onDragEnter
                                }
                            }
                        }}
-                       onBlur={() => {
-                           if (!rank || rank === '-') {
-                               setRank(item.rank);
-                           } else {
-                               rerank([item], rank);
-                           }
-                       }}
+                       onBlur={() => setRank(item.rank)}
                 />
 
                 <button onClick={(event) => {
