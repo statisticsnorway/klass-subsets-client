07eef932966894662c35cfbdf5c3b59ae0e7b2d7
kons_lapi <alina.lapina@ssb.no>
2020-05-25 17:57:12 +0200

    validation: Period cannot include and exclude the same day

--- src/controllers/validator.js
+++ src/controllers/validator.js
@@ -2,6 +2,8 @@ export const validate = {
     period(from, to) {
         return from > to
             ? ['Period cannot start later than end']
-            : [];
+            : from === to
+                ? ['Period cannot include and exclude the same day']
+                : [];
     }
 }
