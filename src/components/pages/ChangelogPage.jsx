818c7e832c67c643a3874d7f6ec26bf41dec4eb1
kons_lapi <alina.lapina@ssb.no>
2020-07-02 14:06:26 +0200

    changelog

--- src/components/pages/ChangelogPage.jsx
+++ src/components/pages/ChangelogPage.jsx
@@ -23,8 +23,9 @@ export default function ChangelogPage() {
 
            <Title size={2}>{t('Changelog')}</Title>
 
-           <Title size={4}>v0.4.2</Title>
+           <Title size={4}>v0.4.3</Title>
            <Paragraph>Fixed empty <code>validUntil</code> and <code>createdDate</code> in order to save and publish subsets.</Paragraph>
+           <Paragraph>Right service used for search from with no until date.</Paragraph>
 
            <Title size={4}>v0.4.2</Title>
            <Paragraph>Use major version number only.</Paragraph>
