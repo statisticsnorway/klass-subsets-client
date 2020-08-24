4bcab64d609ece71de59e36abacd37e656ee5ff5
kons_lapi <alina.lapina@ssb.no>
2020-08-24 03:32:07 +0200

    commented debug outputs

--- src/controllers/klass-api.jsx
+++ src/controllers/klass-api.jsx
@@ -311,11 +311,11 @@ export function useClassification(id = null) {
     }, [version, setCodesWithNotes]);
 
     /*
-        useEffect(() => console.log({metadata}), [metadata]);
-        useEffect(() => console.log({versions}), [versions]);
-        useEffect(() => console.log({version}), [version]);
-        useEffect(() => console.log({codesWithNotes}), [codesWithNotes]);
+        useEffect(() => console.debug({metadata}), [metadata]);
+        useEffect(() => console.debug({versions}), [versions]);
+        useEffect(() => console.debug({version}), [version]);
+        useEffect(() => console.debug({codesWithNotes}), [codesWithNotes]);
     */
 
-    return {metadata, versions, codesWithNotes, isLoadingVersion};
+    return { metadata, versions, codesWithNotes, isLoadingVersion };
 }
