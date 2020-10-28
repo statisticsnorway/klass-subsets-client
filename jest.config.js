035188a9da768eb1456311baf125e8996529f239
kons_lapi <alina.lapina@ssb.no>
2020-10-28 19:41:29 +0100

    removed ts transform config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
-/* eslint-disable
-*
+/* eslint-disable */
+
+/*
 *  Need this set up in order to public test results and test coverage results to Azure pipelines page.
 *  ref. https://github.com/joachimdalen/blog-resources/blob/master/03-tests-and-coverage/jest.config.js
 *  https://joachimdalen.no/2020/10/11/code-coverage-in-azure-pipelines
@@ -8,9 +9,9 @@
 module.exports = {
     roots: ['<rootDir>/src'],
     testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
-    transform: {
+/*    transform: {
         '^.+\\.(ts|tsx)$': 'ts-jest'
-    },
+    },*/
     moduleNameMapper: {
         '\\.(css)$': '<rootDir>/src/__mocks__/styleMock.js'
     },
