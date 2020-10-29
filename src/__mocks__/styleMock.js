e746a5f1095dbc2954269626f926cfa060e21585
kons_lapi <alina.lapina@ssb.no>
2020-10-29 11:01:19 +0100

    test:ci

--- src/__mocks__/styleMock.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-
-/*
-Need this set up in order to public test results and test coverage results to Azure pipelines page.
-ref. https://github.com/joachimdalen/blog-resources/blob/master/03-tests-and-coverage/jest.config.js
-https://joachimdalen.no/2020/10/11/code-coverage-in-azure-pipelines
-*/
-
-module.exports = {};
