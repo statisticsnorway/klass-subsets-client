81fde3c24567d39c6ddcb6cb40635965049b3afc
kons_lapi <alina.lapina@ssb.no>
2020-11-30 13:56:13 +0100

    block expandable

--- src/components/BlockExpandable/BlockExpandable.jsx
+++ src/components/BlockExpandable/BlockExpandable.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import './block-expandable.css';
 
-export const BlockExpandable = ({ label, text, color }) => {
+export const BlockExpandable = ({ label, text, color, light }) => {
     const { t } = useTranslation();
-console.log({text})
     return (
-        <div className='grow' >
-            <span className='label'>{ t( label ) }:</span>
-            <span className='text'>{ t( text ) }</span>
+        <div className='grow'
+             style={{ backgroundColor: color || '#C3DCDC' }}
+        >
+            <span className='label'
+                  style={{ backgroundColor: '#F0F8F9' }}
+            >{ t( label ) }:</span>
+            <span className={ light && 'light' } >{ text }</span>
         </div>
     );
 }
