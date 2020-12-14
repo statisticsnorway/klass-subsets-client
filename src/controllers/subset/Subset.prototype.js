6cf534d6625e3218a8111cfb5267caee46f481d2
kons_lapi <alina.lapina@ssb.no>
2020-12-15 00:22:48 +0100

    nameControl

--- src/controllers/subset/Subset.prototype.js
+++ src/controllers/subset/Subset.prototype.js
@@ -1,4 +1,5 @@
 import { subsetDraft, STATUS_ENUM, languages, acceptablePeriod } from '../../defaults';
+import { nameControl } from './nameControl';
 import { errorsControl } from './errorsControl';
 import { versionable } from './versionsControl';
 import { toId, sanitize, nextDefaultName, orderByValidFromAsc, orderByValidFromDesc, sanitizeArray } from '../../utils';
@@ -440,62 +441,6 @@ const restrictable = (state = {}) => ({
         return (-1 !== languages
             .filter(l => l.draft)
             .findIndex(l => l.languageCode === lang));
-    }
-});
-
-const nameControl = (state = {}) => ({
-
-    addName(name = nextDefaultName(state?.name)) {
-
-        if (state.isEditableName()
-            && state?.name?.length < languages.filter(l => l.draft).length)
-        {
-            //console.debug('addName', name);
-
-            state.name = [...state.name, name];
-        }
-    },
-
-    removeNameByIndex(index) {
-        if (state.isEditableName()
-            && index >= 0 && index < state.name?.length)
-        {
-            //console.debug('removeNameByIndex', index);
-
-            state.name = state.name?.filter((item, i) => i !== index);
-        }
-    },
-
-    removeEmptyNames() {
-        if (state.isEditableName()) {
-            //console.debug('removeEmptyNames');
-
-            state.name = state.name?.filter(item => item.languageText?.length > 0);
-        }
-    },
-
-    updateNameTextByIndex(index = -1, text = '') {
-        if (state.isEditableName()
-            && index >= 0 && index < state.name?.length)
-        {
-            //console.debug('updateNameTextByIndex', index, text);
-
-            state._name[index].languageText = sanitize(text, subsetDraft?.maxLengthName);
-            if (!state.shortName && state.name?.length > 0) {
-                state.id = toId(state.name[0].languageText);
-            }
-        }
-    },
-
-    updateNameLanguageByIndex(index = -1, lang = '') {
-        if (state.isEditableName()
-            && index >= 0 && index < state.name?.length
-            && state.isAcceptableLanguageCode(lang))
-        {
-            //console.debug('updateNameLanguageByIndex', index, lang);
-
-            state._name[index].languageCode = lang;
-        }
     }
 });
 
---
+++ src/controllers/subset/nameControl.js
@@ -0,0 +1,58 @@
+import {nextDefaultName, sanitize, toId} from "../../utils";
+import {languages, subsetDraft} from "../../defaults";
+
+export const nameControl = (state = {}) => ({
+
+    addName(name = nextDefaultName(state?.name)) {
+
+        if (state.isEditableName()
+            && state?.name?.length < languages.filter(l => l.draft).length)
+        {
+            //console.debug('addName', name);
+
+            state.name = [...state.name, name];
+        }
+    },
+
+    removeNameByIndex(index) {
+        if (state.isEditableName()
+            && index >= 0 && index < state.name?.length)
+        {
+            //console.debug('removeNameByIndex', index);
+
+            state.name = state.name?.filter((item, i) => i !== index);
+        }
+    },
+
+    removeEmptyNames() {
+        if (state.isEditableName()) {
+            //console.debug('removeEmptyNames');
+
+            state.name = state.name?.filter(item => item.languageText?.length > 0);
+        }
+    },
+
+    updateNameTextByIndex(index = -1, text = '') {
+        if (state.isEditableName()
+            && index >= 0 && index < state.name?.length)
+        {
+            //console.debug('updateNameTextByIndex', index, text);
+
+            state._name[index].languageText = sanitize(text, subsetDraft?.maxLengthName);
+            if (!state.shortName && state.name?.length > 0) {
+                state.id = toId(state.name[0].languageText);
+            }
+        }
+    },
+
+    updateNameLanguageByIndex(index = -1, lang = '') {
+        if (state.isEditableName()
+            && index >= 0 && index < state.name?.length
+            && state.isAcceptableLanguageCode(lang))
+        {
+            //console.debug('updateNameLanguageByIndex', index, lang);
+
+            state._name[index].languageCode = lang;
+        }
+    }
+});
