a5f077ade5e95ce55df1415e235c940a5b5b4d86
kons_lapi <alina.lapina@ssb.no>
2021-01-24 02:26:02 +0100

    disabled refresh on search subsets page

--- src/controllers/subsets-api.jsx
+++ src/controllers/subsets-api.jsx
@@ -13,7 +13,7 @@ const fullVersions = 'includeFullVersions=true';
 export function useSubsets() {
     const { data, error } = useSWR(
         `${ subsetsServiceEndpoint }?${ defaultQuery }`,
-        fetcher, { revalidateOnReconnect: true }
+        fetcher
     );
     return [ data, error ];
 }
