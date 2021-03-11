cb8baded8c85e34d45f9d471c0aac3b9685a4f13
rsassb <rsa@ssb.no>
2021-03-11 10:00:55 +0100

    Tried another way to sort arrays and set default version - the one with the latest/highest valdiFrom

--- src/utils/arrays.js
+++ src/utils/arrays.js
@@ -9,16 +9,18 @@ export function flatten(arr) {
 
 // TESTME
 export function orderByValidFromDesc(array) {
-    return array?.sort((a,b) =>
-        a.validFrom < b.validFrom ? 1 :
-            a.validFrom > b.validFrom ? -1 : 0);
+    return array?.sort(compareValues('validFrom', 'desc'))
+    // return array?.sort((a,b) =>
+    //     a.validFrom < b.validFrom ? 1 :
+    //         a.validFrom > b.validFrom ? -1 : 0);
 }
 
 // TESTME
 export function orderByValidFromAsc(array) {
-    return array?.sort((a,b) =>
-        a.validFrom < b.validFrom ? -1 :
-            a.validFrom > b.validFrom ? 1 : 0);
+    return array?.sort(compareValues('validFrom'))
+    // return array?.sort((a,b) =>
+    //     a.validFrom < b.validFrom ? -1 :
+    //         a.validFrom > b.validFrom ? 1 : 0);
 }
 
 // TESTME
@@ -27,4 +29,20 @@ export function sanitizeArray(array, maxlength) {
         ...o,
         languageText: sanitize(o.languageText, maxlength)
     }));
+}
+
+
+function compareValues(key, order = 'asc') {
+    return function innerSort(a, b) {
+        if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+            // property doesn't exist on either object
+            console.log(key, "key does not exists")
+            return 0;
+        }
+
+        let comparison = (a[key] > b[key]) ? 1 :((a[key] < b[key]) ? -1 : 0)
+        // console.log(order + ': a=' + a[key] + ', b=' + b[key] + ', comparison=' + comparison +
+        //   ', return ' + ((order === 'desc') ? (comparison * -1) : comparison ))
+        return ( (order === 'desc') ? (comparison * -1) : comparison );
+    };
 }
